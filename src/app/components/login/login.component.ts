a9fb751047cd7399ce59d663d2e4a669f4573c84
Luis <luisjin9@gmail.com>
2022-04-13 19:48:40 +0200

    login and register front done

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+
+  login() {
+    this.submitted = true;
+    if (!this.loginForm.valid) return;
+    console.log('Enviado');
+  }
+}
